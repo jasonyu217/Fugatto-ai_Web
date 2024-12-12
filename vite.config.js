9f0c0fa8addfe307e347663c647eb4ba0a9471e1
JasonYu <546212419@qq.com>
2024-12-12 22:44:09 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [],
@@ -11,9 +13,9 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
+        tailwindcss(),
+        autoprefixer(),
       ],
     },
   },
-}) 
+});
