9f8159e6701124de6567d1b1abf93f46f68a0e50
JasonYu <546212419@qq.com>
2024-12-17 21:28:35 +0800

    api

---
+++ api/test-callback.js
@@ -0,0 +1,16 @@
+export const config = {
+  runtime: 'edge',
+};
+
+export default async function handler(request) {
+  return new Response(
+    JSON.stringify({ message: 'API 工作正常' }), 
+    { 
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    }
+  );
+} 
