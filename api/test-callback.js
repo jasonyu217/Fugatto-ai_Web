15dcde2aa5abc848c1f91f8fc86c996abf2e19e1
JasonYu <546212419@qq.com>
2024-12-17 22:17:03 +0800

    Update test-callback.js

--- api/test-callback.js
+++ api/test-callback.js
@@ -1,9 +1,12 @@
+import { NextResponse } from 'next/server';
+
 export const config = {
   runtime: 'edge',
+  regions: ['sin1']
 };
 
 export default async function handler(request) {
-  return new Response(
+  return new NextResponse(
     JSON.stringify({ message: 'API 工作正常' }), 
     { 
       status: 200,
